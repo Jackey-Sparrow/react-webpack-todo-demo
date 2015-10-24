2609f37aec852b4e4de594ac3f93ab0144924835
Jackey.Sparrow <1109952026@qq.com>
2015-10-24 22:37:30 +0800

    add some style

--- app/dash/components/todo-list.js
+++ app/dash/components/todo-list.js
@@ -11,12 +11,23 @@ export default class TodoList extends React.Component {
     render() {
         var todoList = this.todoList.map(function (todo) {
             return (
-                <div key={todo.id}>{todo.description} {todo.date}</div>
+                <div className='todo-item' key={todo.id}>
+                    <div className='todo-id todo-field'>{todo.id}</div>
+                    <div className='todo-description todo-field'>{todo.description}</div>
+                    <div className='todo-date todo-field'>{todo.date}</div>
+                </div>
             );
         });
 
         return (
-            <div className='todo-lists'>{todoList}</div>
+            <div className='todo-lists'>
+                <div className='todo-item title'>
+                    <div className='todo-id todo-field'>序号</div>
+                    <div className='todo-description todo-field'>未做事项</div>
+                    <div className='todo-date todo-field'>时间</div>
+                </div>
+                {todoList}
+            </div>
         );
     }
 }
