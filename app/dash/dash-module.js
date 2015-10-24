65a5bd1888c8ffe9c0dea4836fbe49c13e3fb1e8
Jackey.Sparrow <1109952026@qq.com>
2015-10-24 18:17:59 +0800

    implement todoList

--- app/dash/dash-module.js
+++ app/dash/dash-module.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Header from './../common/components/header';
+import './content/css/dash.css';
+import TodoList from './services/todo-list-service';
 
 export default class DashComponent extends React.Component {
+    //do not use getInitialState
+    constructor(props) {
+        super(props);
+        this.todoList = new TodoList().getTodoList()
+    }
+
     render() {
+        var todoList = this.todoList.map(function (todo) {
+            return (
+                <div>{todo.description}</div>
+            );
+        });
         return (
             <div>
                 <Header/>
 
-                dash
+                <div className='todo-container'>
+                    <h2>Todo List:</h2>
+
+                    <div>{todoList}</div>
+                </div>
             </div>
         )
     }
---
+++ app/dash/services/todo-list-service.js
@@ -0,0 +1,22 @@
+class TodoList {
+    constructor() {
+        this.todoList = [
+            {
+                id: 1,
+                description: 'buy some sugar',
+                date: '2015-10-24'
+            },
+            {
+                id: 2,
+                description: 'practise es6 more',
+                date: '2015-10-24'
+            }
+        ];
+    }
+
+    getTodoList() {
+        return this.todoList;
+    }
+}
+
+export default TodoList;
